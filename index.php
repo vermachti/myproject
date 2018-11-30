2f93bdc6b6296f7f192f6870f303f04c53d891ab
misanko <misanko@misanko-F5R>
2018-11-30 20:46:14 +0100

    naucil som sa rozbehat ako zapisovat veci do filesystemu, hoci nie uploadovat fajly

--- index.php
+++ index.php
@@ -1,4 +1,15 @@
+<?php 
+?>
+<html>
+<b>HOME</b>
+</br>
+<a href="stiahni.php"><i>stiahni</i></a>
+</br>
+<form method="POST" ENCTYPE="multipart/form-data" action="upload.php">
+<input type="input" name="fileToUpload" id="fileToUpload">
+</br>
+<input type="submit" name="submit" value="Upload File"> 
+</form>
+</html>
 <?php
-
-	echo "test";
 ?>
