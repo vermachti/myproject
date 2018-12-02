0867c3960c1ebbfc63233582fb074e00fc612786
misanko <misanko@misanko-F5R>
2018-12-02 16:14:36 +0100

    co to robi: zobrazi generovany subor do obsahu stranky
    co sa opravilo:
    
    vedlajsie ucinky:

--- upload.php
+++ upload.php
@@ -1,8 +1,8 @@
 <?php
-echo $_POST["fileToUpload"];
+//echo $_POST["fileToUpload"]; //namiesto toho requirenem originalnu stranku
 $string = $_POST["fileToUpload"];
 $fp = fopen ("test.txt", "a+"); 
-fwrite ($fp, $_POST["fileToUpload"] . "\r\n"); 
+fwrite ($fp, $_POST["fileToUpload"] . "\r\n"."</br>"); 
 fclose ($fp); 
-
+require("index.php");//namiesto echa to reloadne originalnu stranku
 ?>
