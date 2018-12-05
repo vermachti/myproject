09e3227f3e9e156c1e3a1d447ae1623b4ba1145d
misanko <michal.rodef@gmail.com>
2018-12-05 15:51:22 +0100

    co to robi: prispevky sa zapisuju do databazy a vypluje sa obsah vsekych prispevkov
    co sa opravilo:
    
    vedlajsie ucinky:

--- index.php
+++ index.php
@@ -1,17 +1,48 @@
 <?php 
+$dbhost = 'localhost';
+$username = 'root';
+$password = '';
+$dbname = 'portfolio';
+$conn = new mysqli($dbhost, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+        die('Connection fialed: ');
+}
+echo 'Connected successfully';
+
+
+$sql = "SELECT id, autor, obsah FROM prispevky";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["id"]. " - Name: " . $row["autor"]. " " . $row["obsah"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+
+
 ?>
 <html>
 <b>HOME</b>
 </br>
 <a href="stiahni.php"><i>stiahni</i></a>
 </br>
-<form method="POST" ENCTYPE="multipart/form-data" action="upload.php">
+<form method="POST" ENCTYPE="multipart/form-data" action="login.php">
 <input type="input" name="fileToUpload" id="fileToUpload">
 </br>
 <input type="submit" name="submit" value="Upload File"> 
 </form>
 
 </html>
-<?php
-readfile("test.txt");
-?>
+
+
+
+
+
+
+
+
