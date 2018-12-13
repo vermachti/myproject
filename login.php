6449da88764ad6e971278d2011e6ae3a0e1384da
misanko <michal.rodef@gmail.com>
2018-12-13 21:26:22 +0100

    co to robi: uz som zabudol co to opravilo: nic vedlajsie ucinky: neviem

--- login.php
+++ login.php
@@ -6,16 +6,9 @@ $dbname = 'portfolio';
 $conn = new mysqli($dbhost, $username, $password, $dbname);
 
 
-if ($conn->connect_error ) {
-	die("Connection fialed: " . $conn->connect_error);
-}
-echo "Connected successfully";
-$sql = "insert into prispevky (autor, obsah) values (1, 'toto je obsah')";
-if ($conn->query($sql) === TRUE) {
-	echo "New record created succcessfully";
-} else {
-	echo "Error: " . $sql . "</br>" . $conn->error;
-}
+$sql = "insert into prispevky (autor, obsah) values (1, '" . $_POST["fileToUpload"] . "')";
+$conn->query($sql);
 $conn->close();
+require("index.php");//namiesto echa to reloadne originalnu stranku
 
 ?>
