f3ad346f52e285b457b4be6570daa59fa9436bad
vermachti <michal.rodef@gmail.com>
2018-12-16 06:46:59 +0100

    zmena formatovania podla standardu a hack unix/dos formatovania

--- rekurzia.php
+++ rekurzia.php
@@ -1,22 +1,24 @@
 <?php
-/**
- * Adresa databazoveho servru
- * 
- * PHP version 7.2.5
- * 
- * @category PHP
- * @package  PHP_CodeSniffer
- * @author   Michal Fedor <michal.rodef@gmail.com>
- * @license  http://matrix.squiz.net/developer/tools/php_cs/licence BSD Licence
- * @link     http://pear.php.net/package/PHP_CodeSniffer
- */
+ /**
+  * Adresa databazoveho servru
+  * 
+  * PHP version 7.2.5
+  * 
+  * @category PHP
+  * @package  PHP_CodeSniffer
+  * @author   Michal Fedor <michal.rodef@gmail.com>
+  * @license  http://matrix.squiz.net/developer/tools/php_cs/licence BSD Licence
+  * @link     http://pear.php.net/package/PHP_CodeSniffer
+  */
+ 
 $dbhost = "localhost";
 $username = "root";
 $password = "ww";
-$dbname = "portfolio";
-$conn = new mysqli($dbhost, $username, $password, $dbname);
-$sql = "SELECT id, autor, obsah FROM prispevky";
-$result = $conn->query($sql);
+$dbname = "portfolio"; 
+$conn = new mysqli($dbhost, $username, $password, $dbname); 
+$sql = "SELECT id, autor, obsah FROM prispevky"; 
+$result = $conn->query($sql); 
+
 /**
  * Rekurzivne fetchovanie databazy
  * 
@@ -31,14 +33,17 @@ function rekurzia()
 {
     $row = $result->fetch_assoc();
     if ($row == 0) {
-        return;
+         return;
     } else {
-        echo "id: " . $row["id"]. " - autor: " . $row["autor"]. " " . $row["obsah"]. "<br>";
-        rekurzia();
+         echo "id: ".$row["id"]. " - autor: ".$row["autor"]." ".$row["obsah"]."<br>";
+         rekurzia();
     }
 }
-rekurzia();
+ rekurzia();
+ 
 
+ 
 $conn->close();
+ 
 ?>
-
+ 
