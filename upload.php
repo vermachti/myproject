939b76deeb7331452cfe693205b2d656bf932aea
misanko <michal.rodef@gmail.com>
2018-12-16 18:30:00 +0100

    bohuzial som zabudol co robi tento komit

--- upload.php
+++ upload.php
@@ -1,7 +1,7 @@
 <?php
 //echo $_POST["fileToUpload"]; //namiesto toho requirenem originalnu stranku
 $string = $_POST["fileToUpload"];
-$fp = fopen ("test.txt", "a+"); 
+$fp = fopen ("test.txt", "a"); 
 fwrite ($fp, $_POST["fileToUpload"] . "\r\n"."</br>"); 
 fclose ($fp); 
 require("index.php");//namiesto echa to reloadne originalnu stranku
