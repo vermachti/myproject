6df1a370354ee403fde461e0c6347274e16cc43d
vermachti <michal.rodef@gmail.com>
2018-12-16 06:52:42 +0100

    removed trash files from repository

--- rekurzia.php
+++ rekurzia.php
@@ -10,40 +10,38 @@
   * @license  http://matrix.squiz.net/developer/tools/php_cs/licence BSD Licence
   * @link     http://pear.php.net/package/PHP_CodeSniffer
   */
- 
-$dbhost = "localhost";
-$username = "root";
-$password = "ww";
-$dbname = "portfolio"; 
-$conn = new mysqli($dbhost, $username, $password, $dbname); 
-$sql = "SELECT id, autor, obsah FROM prispevky"; 
-$result = $conn->query($sql); 
 
-/**
- * Rekurzivne fetchovanie databazy
- * 
- * PHP version 7.2.5
- * 
- * @category PHP
- * @package  PHP_CodeSniffer
- * @author   Michal Fedor <michal.rodef@gmail.com>
- * @return   void
- */
+  $dbhost = "localhost";
+  $username = "root";
+  $password = "ww";
+  $dbname = "portfolio"; 
+  $conn = new mysqli($dbhost, $username, $password, $dbname); 
+  $sql = "SELECT id, autor, obsah FROM prispevky"; 
+  $result = $conn->query($sql); 
+  /**
+   * Rekurzivne fetchovanie databazy
+   * 
+   * PHP version 7.2.5
+   * 
+   * @category PHP
+   * @package  PHP_CodeSniffer
+   * @author   Michal Fedor <michal.rodef@gmail.com>
+   * @return   void
+   */
 function rekurzia()
 {
     $row = $result->fetch_assoc();
     if ($row == 0) {
-         return;
+            return;
     } else {
-         echo "id: ".$row["id"]. " - autor: ".$row["autor"]." ".$row["obsah"]."<br>";
-         rekurzia();
+        echo "id: ".$row["id"]. " - autor: ".$row["autor"]." ".$row["obsah"]."<br>";
+        rekurzia();
     }
 }
- rekurzia();
- 
-
- 
-$conn->close();
- 
+     rekurzia();
+     
+     
+     
+     $conn->close();
+     
 ?>
- 
