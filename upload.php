6c346e8fa8840166764628530945f898e83b8ce5
vermachti <michal.rodef@gmail.com>
2018-12-16 06:52:42 +0100

    removed trash files from repository

--- upload.php
+++
@@ -1,8 +0,0 @@
-<?php
-//echo $_POST["fileToUpload"]; //namiesto toho requirenem originalnu stranku
-$string = $_POST["fileToUpload"];
-$fp = fopen ("test.txt", "a"); 
-fwrite ($fp, $_POST["fileToUpload"] . "\r\n"."</br>"); 
-fclose ($fp); 
-require("index.php");//namiesto echa to reloadne originalnu stranku
-?>
