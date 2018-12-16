5e92b81e18ae809ba6554fede3d057114b9ef0a6
vermachti <michal.rodef@gmail.com>
2018-12-16 06:52:42 +0100

    removed trash files from repository

--- stiahni.php
+++
@@ -1,9 +0,0 @@
-<?php 
-$soubor = "README.md"; 
-
-header("Content-Description: File Transfer"); 
-header("Content-Type: application/force-download"); 
-header("Content-Disposition: attachment; filename=\"$soubor\""); 
-
-readfile ($soubor); 
-?>
